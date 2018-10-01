71694374b7bf858be33ec2f64da7dbf4c868b979
Diogo Cardoso <diogocardoso92@outlook.com>
2018-10-01 12:44:31 +0100

    Update hero/particles for mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import Particles from 'react-particles-js';
 import Typing from 'react-typing-animation';
-import { particlesCfg } from '../config/index';
+import MediaQuery from 'react-responsive';
+import { particlesCfgMobile, particlesCfgDesktop, particlesStyle } from '../config/index';
 
 const HeroContainer = styled.section`
   background-color: #747FE0;
@@ -10,6 +11,10 @@ const HeroContainer = styled.section`
   margin-top: 100px;
   box-sizing: border-box;
   height: calc(100vh - 20rem);
+  @media (max-width: 1024px) {
+    margin-top: 70px;
+    height: calc(100vh - 70px);
+  }
 `;
 
 const HeroOverlay = styled.div`
@@ -18,18 +23,31 @@ const HeroOverlay = styled.div`
   left: 360px;
   margin: auto;
   color: white;
+  /* TODO: Fix positioning */
+  @media (max-width: 1024px) {
+    top: 60%;
+    left: 5%;
+    right: 5%;
+  }
 `;
 
 const Hero = () => (
   <HeroContainer>
-    <Particles height='100%' params={particlesCfg} />
+    <MediaQuery minWidth={1024}>
+      {matches => {
+        return matches
+          ? <Particles style={particlesStyle} params={particlesCfgDesktop} />
+          : <Particles style={particlesStyle} params={particlesCfgMobile} />
+      }}
+    </MediaQuery>
+    {/* <Particles style={particlesStyle} params={particlesCfg} /> */}
     <HeroOverlay>
       <Typing>
         <span>Hi, I'm <strong>Diogo</strong>.</span>
         <br />
         <span>I'm a <strong>full-stack JavaScript web developer</strong>. Based in Porto, Portugal.</span>
-        <br />
-        <span>Currently in the final-year of a master's degree in Electrical and Computers Engineering at FEUP.</span>
+        {/* <br />
+        <span>Currently in the final-year of a master's degree in Electrical and Computers Engineering at FEUP.</span> */}
       </Typing>
     </HeroOverlay>
   </HeroContainer>
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,79 @@
-export const particlesCfg = {
+export const particlesStyle = {
+  bottom: 0,
+  left: 0,
+  position: 'absolute',
+  right: 0,
+  top: 0
+};
+
+export const particlesCfgMobile = {
+  particles: {
+    color: {
+      value: '#FFFFFF',
+    },
+    line_linked: {
+      color: '#FFFFFF',
+      distance: 150,
+      enable: true,
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+      bounce: false,
+      direction: 'none',
+      enable: true,
+      out_mode: 'out',
+      random: false,
+      speed: 6,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+      value: 20,
+    },
+    opacity: {
+      anim: {
+        enable: false,
+        opacity_min: 0.1,
+        speed: 1,
+        sync: false,
+      },
+      random: false,
+      value: 0.5,
+    },
+    shape: {
+      polygon: {
+        nb_sides: 5,
+      },
+      stroke: {
+        color: '#000000',
+        width: 0,
+      },
+      type: 'circle',
+    },
+    size: {
+      anim: {
+        enable: false,
+        size_min: 0.1,
+        speed: 40,
+        sync: false,
+      },
+      random: true,
+      value: 3,
+    },
+  },
+  retina_detected: true,
+};
+
+export const particlesCfgDesktop = {
   particles: {
     color: {
       value: '#FFFFFF',
