e72ac94a46abf9206d79a0b64bf5de0412e64097
Diogo Cardoso <diogocardoso92@outlook.com>
2018-10-03 02:13:20 +0100

    Update styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,16 +5,19 @@ const Wrapper = styled.section`
   width: 100%;
   padding: 0 15%;
   height: 50px;
-  margin: 6rem auto 0 auto;
+  margin: 7rem auto 0 auto;
   text-align: center;
   background-color: #F6F9FC;
   box-sizing: border-box;
+  @media (max-width: 1024px) {
+    margin-top: 4rem;
+  }
 `;
 
 const Text = styled.h3`
   margin: 0;
   padding: 0;
-  border-top: 2px solid rgba(166,111,237,.3);
+  border-top: 2px solid rgba(166, 111, 237, .3);
   padding-top: 1rem;
 `;
 
