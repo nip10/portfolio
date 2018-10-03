0f1088de5975d9ae61293ba1d213bae8c34f10d5
Diogo Cardoso <diogocardoso92@outlook.com>
2018-10-03 02:35:45 +0100

    Add project images

--- src/projects/index.ts
+++ src/projects/index.ts
@@ -1,7 +1,17 @@
 import api from './../assets/img/projects/api.svg';
 import chat from './../assets/img/projects/chat.png';
+import todo from './../assets/img/projects/todo.png';
+import portfolio from './../assets/img/projects/portfolio.png';
 
 export const projects = [
+  {
+    title: 'Todo\'s Manager',
+    description: 'Front-end for managing todo\'s.',
+    stack: ['HTML5', 'CSS3', 'Typescript', 'React'],
+    image: todo,
+    repo: 'https://github.com/nip10/todo-react-ts',
+    demo: 'https://p.dcdev.pt/todo',
+  },
   {
     title: 'Todo api',
     description: 'A RESTful API for managing todo\'s using all CRUD common operations.',
@@ -9,14 +19,6 @@ export const projects = [
     image: api,
     repo: 'https://github.com/nip10/todo-api',
     demo: 'https://p.dcdev.pt/todo/api',
-  },
-  {
-    title: 'Todo\'s Manager',
-    description: 'Front-end for managing todo\'s.',
-    stack: ['HTML5', 'CSS3', 'Typescript', 'React'],
-    image: 'https://via.placeholder.com/400x400',
-    repo: 'https://github.com/nip10/todo-react-ts',
-    demo: 'https://p.dcdev.pt/todo',
   },
   {
     title: 'Chat',
@@ -45,7 +47,7 @@ export const projects = [
     title: 'Portfolio',
     description: 'This website. SPA built with React and Styled Components.',
     stack: ['Typescript', 'React', 'CSS3'],
-    image: 'https://via.placeholder.com/400x400',
+    image: portfolio,
     repo: 'https://github.com/nip10/portfolio',
     demo: 'https://dcdev.pt',
   },
