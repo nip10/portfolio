d73fd13a75909244d4eea4a2564f42103f7550fd
Diogo Cardoso <diogocardoso92@outlook.com>
2018-10-03 02:17:03 +0100

    Move tech stack information to its own file

---
+++ src/stack/index.ts
@@ -0,0 +1,75 @@
+const frontEnd = [
+  {
+    name: 'HTML5',
+  },
+  {
+    name: 'CSS3',
+  },
+  {
+    name: 'Sass',
+  },
+  {
+    name: 'React',
+    alias: 'r',
+  },
+  {
+    name: 'Redux',
+  },
+  {
+    name: 'SocketIO',
+  },
+];
+
+const backEnd = [
+  {
+    name: 'NodeJS',
+  },
+  {
+    name: 'MongoDB',
+  },
+  {
+    name: 'PostgreSQL',
+  },
+  {
+    name: 'Redis',
+  },
+];
+
+const languages = [
+  {
+    name: 'JavaScript',
+  },
+  {
+    name: 'TypeScript',
+  },
+  {
+    name: 'C++',
+    alias: 'cplusplus',
+  },
+  {
+    name: 'Matlab',
+  },
+];
+
+const tools = [
+  {
+    name: 'Git',
+  },
+  {
+    name: 'npm',
+  },
+  {
+    name: 'Yarn',
+  },
+  {
+    name: 'Gulp',
+  },
+  {
+    name: 'Webpack',
+  },
+  {
+    name: 'Jest',
+  },
+];
+
+export default { frontEnd, backEnd, tools, languages };
